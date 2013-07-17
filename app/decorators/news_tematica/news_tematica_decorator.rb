e6afe1f33e26fa8d3a6a8361116d8be4975d5386
Fernan2 <fernandorankia@gmail.com>
2013-07-17 20:15:39 +0200

    Coge contenidos sin filtrar por temática cuando sea la news general

--- app/decorators/news_tematica/news_tematica_decorator.rb
+++ app/decorators/news_tematica/news_tematica_decorator.rb
@@ -24,15 +24,16 @@ module NewsTematica
         taggings = tagging_class.where(tag_id: source.tematica.tag_id).where(created_at: source.fecha_desde..source.fecha_hasta).where(fecha_titulares: source.fecha_desde..source.fecha_hasta)
         q = contenido_class.where(id: taggings.map(&:taggable_id))
       else
-        antifail_class.create(tipo: 'Temática sin titulares configurados', detalles: source.tematica.inspect)
-        return nil
+        q = contenido_class.where(created_at: source.fecha_desde..source.fecha_hasta).where(fecha_titulares: source.fecha_desde..source.fecha_hasta)
       end
       prioriza q.publicado.in_locale('es').includes(:fotos, :veces_leido, :blog).all
     end
 
     # Lo más leído se apoyará en los scopes de las portadas temáticas
     def lo_mas_leido
-      contenido_class.publicado.in_locale('es').includes(:veces_leido, :blog).where(created_at: source.fecha_desde..source.fecha_hasta).send("de_#{ source.tematica.scope_mas_leido }".to_sym).all.sort_by { |msg| 100 - msg.veces_leido.contador * msg.factor_corrector_para_nuevos }
+      msgs = contenido_class.publicado.in_locale('es').includes(:veces_leido, :blog).where(created_at: source.fecha_desde..source.fecha_hasta)
+      msgs = msgs.send("de_#{ source.tematica.scope_mas_leido }".to_sym) if source.tematica.scope_mas_leido.present?
+      msgs.all.sort_by { |msg| 100 - (msg.veces_leido ? msg.veces_leido.contador : 0) * msg.factor_corrector_para_nuevos }
     end
 
     # Los foros pueden ser foros principales (basados en subtipo_id) o foros temáticos (basados en tag_id)
@@ -43,8 +44,7 @@ module NewsTematica
         taggings = tagging_class.where(tag_id: source.tematica.tag_id).where(tema: Subtipo::ARRAY_FOROS_NORMALES).where(created_at: source.fecha_desde..source.fecha_hasta)
         q = contenido_class.where(id: taggings.map(&:taggable_id))
       else
-        antifail_class.create(tipo: 'Temática sin foro configurado', detalles: source.tematica.inspect)
-        return nil
+        q = contenido_class.where(tema: Subtipo::ARRAY_FOROS_NORMALES).where(created_at: source.fecha_desde..source.fecha_hasta)
       end
       prioriza q.publicado.in_locale('es').includes(:veces_leido, :blog).all
     end
