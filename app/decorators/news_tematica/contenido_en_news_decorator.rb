bbfe801893b145838e0019ee7e3779657751f27b
esaborit4code <edu@emergia.net>
2013-08-02 14:01:30 +0200

    Arreglo del fallo provocado por no namespacear ContenidoEnNewsDecortador

--- app/decorators/news_tematica/contenido_en_news_decorator.rb
+++ app/decorators/news_tematica/contenido_en_news_decorator.rb
@@ -1,9 +1,11 @@
 # coding: UTF-8
 
-class ContenidoEnNewsDecorator < Draper::Decorator
-  delegate_all
+module NewsTematica
+  class ContenidoEnNewsDecorator < Draper::Decorator
+    delegate_all
 
-  def foto_para_news
-    h.image_tag(fotos.first.adjunto.url(:col), alt: fotos.first.titulo) if fotos.first
+    def foto_para_news
+      h.image_tag(fotos.first.adjunto.url(:col), alt: fotos.first.titulo) if fotos.first
+    end
   end
 end
