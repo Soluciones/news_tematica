d012fdb8b5d7a61cc14c889fd60de5c154e20748
David BF <burretfresket@gmail.com>
2015-03-24 09:59:53 +0100

    Todas -> Todos & unhounding

--- db/migrate/20150323104957_add_dominio_de_envio_to_news_tematicas.rb
+++ db/migrate/20150323104957_add_dominio_de_envio_to_news_tematicas.rb
@@ -1,5 +1,5 @@
 class AddDominioDeEnvioToNewsTematicas < ActiveRecord::Migration
   def change
-    add_column :news_tematica_news_tematicas , :dominio_de_envio, :string
+    add_column :news_tematica_news_tematicas, :dominio_de_envio, :string
   end
 end
