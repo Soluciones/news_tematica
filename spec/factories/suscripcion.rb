def62c416d0c478673a393f9ebc515eb89ee2f56
David BF <burretfresket@gmail.com>
2015-03-24 10:20:38 +0100

    Clean Suscripcion factory associations, as aliases are created

--- spec/factories/suscripcion.rb
+++ spec/factories/suscripcion.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :suscripcion, class: Suscribir::Suscripcion do
-    association :suscribible, factory: :contenido
-    association :suscriptor, factory: :usuario
+    association :suscribible
+    association :suscriptor
     email { suscriptor.email }
     dominio_de_alta 'es'
   end
