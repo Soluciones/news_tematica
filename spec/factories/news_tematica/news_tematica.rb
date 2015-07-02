6c5c39bc946e4df54dcc680985b27939b21b66e8
David BF <davidbf@rankia.com>
2015-07-02 17:43:38 +0200

    Remove ApplicationHelper tests & fix news_tematica factory

--- spec/factories/news_tematica/news_tematica.rb
+++ spec/factories/news_tematica/news_tematica.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :news_tematica, class: NewsTematica::NewsTematica do
-    tematica      { FactoryGirl.create(:tematica) }
-    titulo        { "Novedades de #{ tematica.nombre }" }
-    fecha_envio   { 1.day.from_now }
-    html          { "<h1>Hello world!</h1>" }
+    suscribible { FactoryGirl.create(:tematica) }
+    titulo { "Novedades de #{ tematica.nombre }" }
+    fecha_envio { 1.day.from_now }
+    html '<h1>Hello world!</h1>'
     banner_1_url_imagen { FFaker::Internet.uri 'http' }
     banner_1_texto_alt { FFaker::Lorem.sentence }
     banner_1_url_destino { FFaker::Internet.uri 'http' }
     banner_2_url_imagen { FFaker::Internet.uri 'http' }
-    banner_2_texto_alt  { FFaker::Lorem.sentence }
+    banner_2_texto_alt { FFaker::Lorem.sentence }
     banner_2_url_destino { FFaker::Internet.uri 'http' }
   end
 end
--- spec/helpers/news_tematica/application_helper_spec.rb
+++ spec/helpers/news_tematica/application_helper_spec.rb
@@ -1,30 +1,4 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe NewsTematica::ApplicationHelper, type: :helper do
-  describe "link_to_con_estadisticas" do
-    let(:redirection_class) { ::NewsTematica::Clases.redirection_extern.constantize }
-    let(:mi_news_tematica) { create(:news_tematica) }
-    let(:request) {
-      request = double('source')
-      allow(request).to receive(:host).and_return('test.host')
-      request
-    }
-
-    it "debe crear la redirección, si no existe, y devolver enlace que usa la redireccion" do
-      result = link_to_con_estadisticas('Hola Poldo', '/ejemplo', mi_news_tematica, class: 'link-ppal')
-      expect(redirection_class.count).to eq 1
-      expect(redirection_class.where(url: '/ejemplo', news_tematica_id: mi_news_tematica.id).count).to eq 1
-      expect(result).to eq link_to('Hola Poldo', "http://test.host/redirections/#{redirection_class.first.id}",
-                                   class: 'link-ppal')
-    end
-
-    it "debe usar la redirección que ya existe, y devolver enlace que usa la redireccion" do
-      result = link_to_con_estadisticas('Hola Poldo', '/ejemplo', mi_news_tematica, class: 'link-ppal')
-      result2 = link_to_con_estadisticas('Adiós Poldo', '/ejemplo', mi_news_tematica, class: 'pingback')
-      expect(redirection_class.count).to eq 1
-      expect(redirection_class.where(url: '/ejemplo', news_tematica_id: mi_news_tematica.id).count).to eq 1
-      expect(result2).to eq link_to('Adiós Poldo', "http://test.host/redirections/#{redirection_class.first.id}",
-                                    class: 'pingback')
-    end
-  end
+describe NewsTematica::ApplicationHelper do
 end
