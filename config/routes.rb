f024b6f789bdacd22590819904b8f27074fec818
David BF <davidbf@rankia.com>
2016-01-12 12:22:44 +0100

    Move date updates to #elegir_contenidos

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ NewsTematica::Engine.routes.draw do
   resources :news_tematicas do
     post :contenidos_elegidos, on: :member
     get :elegir_contenidos, on: :member
+    patch :update_dates, on: :member
   end
 end
